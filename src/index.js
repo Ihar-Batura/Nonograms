88bb17f4572240e112dd4bcd095466b9bc4cf025
Ihar-Batura <A17331582@gmail.com>
2024-09-05 15:55:02 +0300

    feat: add footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const btnBurger = document.querySelector('.burger-menu');
+const header = document.querySelector('.header-container');
+
+btnBurger.addEventListener('click', () => {
+  btnBurger.classList.toggle('active');
+  header.classList.toggle('active');
+});
